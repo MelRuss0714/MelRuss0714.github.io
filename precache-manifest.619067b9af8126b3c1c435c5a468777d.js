5240ae65c0461a010e8c4244ec92399a1a8e944b
MelRuss0714 <melindasuerussell@yahoo.com>
2018-11-12 18:22:31 -0500

    Updates

---
+++ precache-manifest.619067b9af8126b3c1c435c5a468777d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2b222d94a2c0148ef3e6899146cb715",
+    "url": "/static/media/ProPicture.e2b222d9.jpg"
+  },
+  {
+    "revision": "923722cf549701a5f7bd",
+    "url": "/static/css/main.54f2f96b.chunk.css"
+  },
+  {
+    "revision": "b2394078c14213bf34f9",
+    "url": "/static/js/1.b2394078.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d0aa34c1a77b25b9a5afddb0fac708d9",
+    "url": "/static/media/BubblinkPic.d0aa34c1.png"
+  },
+  {
+    "revision": "70262b52c83d8c223d1e061d50c7c809",
+    "url": "/static/media/SmartRock.70262b52.png"
+  },
+  {
+    "revision": "730d2106dff8813508bf10de4ee167e1",
+    "url": "/static/media/PoppyPic.730d2106.png"
+  },
+  {
+    "revision": "05b3be354073592452196aafbbb0ea76",
+    "url": "/static/media/MelindaRussellResume.05b3be35.docx"
+  },
+  {
+    "revision": "923722cf549701a5f7bd",
+    "url": "/static/js/main.923722cf.chunk.js"
+  },
+  {
+    "revision": "9265a0c32d882930861daac1dc20f39b",
+    "url": "/static/media/crystalClick.9265a0c3.png"
+  },
+  {
+    "revision": "5e994a66759b4af6b20cb2ba649c73dc",
+    "url": "/static/media/trivia.5e994a66.png"
+  },
+  {
+    "revision": "61e56ef39ba33abb6257440934f21141",
+    "url": "/static/media/GifApi.61e56ef3.png"
+  },
+  {
+    "revision": "bc3a501d4db78c71fc95b3e117134875",
+    "url": "/static/media/MelindaRussellResume1.bc3a501d.png"
+  },
+  {
+    "revision": "533be871f06a97cfcb59148a12e4ab35",
+    "url": "/static/media/MelindaRussellResume2.533be871.png"
+  },
+  {
+    "revision": "70972f597fac80e1d1a36bd6f348d730",
+    "url": "/static/media/MelindaRussellResume.70972f59.pdf"
+  },
+  {
+    "revision": "97f48f4a5721d72f6a03df9f97b83986",
+    "url": "/index.html"
+  }
+];
