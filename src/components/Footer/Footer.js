52fbbcdd9c71371786c952806eaac2697655d953
Melinda Russell <melindasuerussell@yahoo.com>
2018-11-12 08:19:49 -0500

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Footer.css';
+
+
+function Footer(props) {
+    return (
+        <footer className="page-footer">
+            <div className="footer-copyright">
+                <p className="links">© 2018 Melinda Russell
+                
+
+                        <i id="firstIcon" className="fab fa-html5"></i>
+                        <i className="fab fa-css3"></i>
+                        <i className="fab fa-font-awesome"></i>
+                        <i className="fab fa-js"></i>
+                        <i className="fab fa-react"></i>
+                  
+                </p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
